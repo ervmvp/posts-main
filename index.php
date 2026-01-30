f59639f16a251834451cd652ccb37d3536cb1d79
ervmvp <hiervin49@gmail.com>
2026-01-30 10:44:10 +0200

    #

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
             $sql_query = "SELECT * FROM posts";
             $params = [];
             if(isset($_GET["search_query"]) && trim($_GET["search_query"]) != "") {
-                $sql_query .= " WHERE content LIKE :search";
+                $sql_query .= "WHERE content LIKE :search";
                 $params["search"] = "%" . $_GET["search_query"] . "%";
             }
         $posts = $db->query($sql_query, $params)->fetchAll(PDO::FETCH_ASSOC);
